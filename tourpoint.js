4d682a143000d8526603dfb19716dbe3861d7a57
Paphawarin258 <124546284+Paphawarin258@users.noreply.github.com>
2023-04-24 14:35:38 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,80 @@
+var tourpoint = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Bangkok",
+          "type": "culture",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            100.4902277914353,
+            13.733586820704872
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Nonthaburi",
+          "type": "natural",
+          "level": 8
+        },
+        "geometry": {
+          "coordinates": [
+            100.48408517762778,
+            13.831305088556277
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Nakhon Pathom",
+          "type": "culture",
+          "level": 9
+        },
+        "geometry": {
+          "coordinates": [
+            100.05472421621567,
+            13.808533682486399
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Samut Sakhon",
+          "type": "culture",
+          "level": 7
+        },
+        "geometry": {
+          "coordinates": [
+            100.27132408278572,
+            13.571917721741102
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Samut Prakan",
+          "type": "culture",
+          "level": 8
+        },
+        "geometry": {
+          "coordinates": [
+            100.54564000673008,
+            13.56832998691931
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
